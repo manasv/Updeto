6c260ca8c3f273fd0b260e7c2c41e31bff2d72df
Manuel Sánchez <manasan93@gmail.com>
2021-05-31 23:47:03 -0600

    test: update tests to use new mock

--- Tests/UpdetoTests/UpdetoTests.swift
+++ Tests/UpdetoTests/UpdetoTests.swift
@@ -7,160 +7,83 @@ import Combine
 #endif
 
 final class UpdetoTests: XCTestCase {
-    private var cancellables = Set<AnyCancellable>()
+    var result: AppStoreLookupResult?
 
-    #if canImport(Combine)
-    func testAppIsUpToDateAndWithoutErrorsOnLookup() {
-        // Given
-        let updeto = UpdetoMock(shouldFail: false, isUpdated: true)
-        var isUpdated = false
-        var lookupError: Error?
-        var streamFinished = false
-
-        // When
-        updeto.isAppUpdated()
-            .sink { completion in
-                switch completion {
-                case .failure(let error):
-                    lookupError = error
-                case .finished:
-                    streamFinished = true
-                }
-            } receiveValue: { value in
-                isUpdated = value
-            }.store(in: &cancellables)
-
-        _ = XCTWaiter.wait(for: [XCTestExpectation()], timeout: 0.2)
-
-        //Then
-        XCTAssertTrue(isUpdated)
-        XCTAssertNil(lookupError)
-        XCTAssertTrue(streamFinished)
+    override func tearDownWithError() throws {
+        result = nil
     }
 
-    func testAppIsNotUpToDateAndWithoutErrorsOnLookup() {
+    func testAppIsUpToDateAndWithoutErrorsOnLookup() throws {
         // Given
-        let updeto = UpdetoMock(shouldFail: false, isUpdated: false)
-        var isUpdated = false
-        var lookupError: Error?
-        var streamFinished = false
+        let updeto = UpdetoMock(bundleId: "com.example.app", currentAppVersion: "1.0.0", responseType: .withResults)
 
         // When
-        updeto.isAppUpdated()
-            .sink { completion in
-                switch completion {
-                case .failure(let error):
-                    lookupError = error
-                case .finished:
-                    streamFinished = true
-                }
-            } receiveValue: { value in
-                isUpdated = value
-            }.store(in: &cancellables)
-
-        _ = XCTWaiter.wait(for: [XCTestExpectation()], timeout: 0.2)
-
-        // Then
-        XCTAssertFalse(isUpdated)
-        XCTAssertNil(lookupError)
-        XCTAssertTrue(streamFinished)
-    }
-
-    func testLookupFailedWithoutResults() {
-        // Given
-        let updeto = UpdetoMock(shouldFail: true, isUpdated: false)
-        var isUpdated = false
-        var lookupError: Error?
-        var streamFinished = false
-
-        // When
-        updeto.isAppUpdated()
-            .sink { completion in
-                switch completion {
-                case .failure(let error):
-                    lookupError = error
-                case .finished:
-                    streamFinished = true
-                }
-            } receiveValue: { value in
-                isUpdated = value
-            }.store(in: &cancellables)
-
-        _ = XCTWaiter.wait(for: [XCTestExpectation()], timeout: 0.2)
-
-        // Then
-        XCTAssertFalse(isUpdated)
-        XCTAssertNotNil(lookupError)
-        XCTAssertFalse(streamFinished)
-    }
-    #endif
-
-    func testAppIsUpToDateAndWithoutErrorsOnLookupWithCompletionBlock() {
-        // Given
-        let updeto = UpdetoMock(shouldFail: false, isUpdated: true)
-        var isUpdated = false
-        var lookupError: Error?
-
-        // When
-        updeto.isAppUpdated { result in
-            switch result {
-            case .success(let value):
-                isUpdated = value
-            case .failure(let error):
-                lookupError = error
+        if #available(iOS 13.0, *) {
+            updeto.isAppUpdated()
+                .compactMap { $0 }
+                .assign(to: \.result, on: self)
+                .cancel()
+        } else {
+            updeto.isAppUpdated { lookupResult in
+                self.result = lookupResult
             }
         }
 
         _ = XCTWaiter.wait(for: [XCTestExpectation()], timeout: 0.2)
 
         //Then
-        XCTAssertTrue(isUpdated)
-        XCTAssertNil(lookupError)
+        let unwrappedResult = try XCTUnwrap(result)
+        let appStoreURL = try XCTUnwrap(updeto.appstoreURL)
+        XCTAssertEqual(unwrappedResult, .updated)
+        XCTAssertEqual(appStoreURL.absoluteString, "itms-apps://apple.com/app/id123456789")
     }
-
-    func testAppIsNotUpToDateAndWithoutErrorsOnLookupWithCompletionBlock() {
+    
+    func testAppIsNotUpDateAndWithoutErrorsOnLookup() throws {
         // Given
-        let updeto = UpdetoMock(shouldFail: false, isUpdated: false)
-        var isUpdated = false
-        var lookupError: Error?
+        let updeto = UpdetoMock(bundleId: "com.example.app", currentAppVersion: "0.0.1", responseType: .withResults)
 
         // When
-        updeto.isAppUpdated { result in
-            switch result {
-            case .success(let value):
-                isUpdated = value
-            case .failure(let error):
-                lookupError = error
+        if #available(iOS 13.0, *) {
+            updeto.isAppUpdated()
+                .compactMap { $0 }
+                .assign(to: \.result, on: self)
+                .cancel()
+        } else {
+            updeto.isAppUpdated { lookupResult in
+                self.result = lookupResult
             }
         }
 
         _ = XCTWaiter.wait(for: [XCTestExpectation()], timeout: 0.2)
 
         //Then
-        XCTAssertFalse(isUpdated)
-        XCTAssertNil(lookupError)
+        let unwrappedResult = try XCTUnwrap(result)
+        let appStoreURL = try XCTUnwrap(updeto.appstoreURL)
+        XCTAssertEqual(unwrappedResult, .outdated)
+        XCTAssertEqual(appStoreURL.absoluteString, "itms-apps://apple.com/app/id123456789")
     }
-
-    func testLookupFailedWithoutResultsWithCompletionBlock() {
+    
+    func testLookupHaveNoResults() throws {
         // Given
-        let updeto = UpdetoMock(shouldFail: true, isUpdated: false)
-        var isUpdated = false
-        var lookupError: Error?
+        let updeto = UpdetoMock(bundleId: "com.example.app", currentAppVersion: "0.0.1", responseType: .noResults)
 
         // When
-        updeto.isAppUpdated { result in
-            switch result {
-            case .success(let value):
-                isUpdated = value
-            case .failure(let error):
-                lookupError = error
+        if #available(iOS 13.0, *) {
+            updeto.isAppUpdated()
+                .compactMap { $0 }
+                .assign(to: \.result, on: self)
+                .cancel()
+        } else {
+            updeto.isAppUpdated { lookupResult in
+                self.result = lookupResult
             }
         }
 
         _ = XCTWaiter.wait(for: [XCTestExpectation()], timeout: 0.2)
 
         //Then
-        XCTAssertFalse(isUpdated)
-        XCTAssertNotNil(lookupError)
+        let unwrappedResult = try XCTUnwrap(result)
+        XCTAssertEqual(unwrappedResult, .noResults)
+        XCTAssertNil(updeto.appstoreURL)
     }
 }
