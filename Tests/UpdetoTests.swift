317e50ece1246622557389da444373e4bffe6ced
Manuel Sanchez <manasan93@gmail.com>
2025-05-25 18:40:16 -0400

    chore: nuke past tests, preparing for new ones

---
+++ Tests/UpdetoTests.swift
@@ -0,0 +1,16 @@
+import XCTest
+@testable import Updeto
+
+final class UpdetoTests: XCTestCase {
+    override func setUp() {
+        super.setUp()
+    }
+
+    override func tearDown() {
+        super.tearDown()
+    }
+
+    func test_example() {
+        XCTAssertEqual("0.0.0", "0.0.0")
+    }
+}
